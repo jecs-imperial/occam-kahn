6190b84904e3911b2bffa62ec25741417c57a236
James Smith <jecs@imperial.ac.uk>
2017-07-14 10:50:14 +0100

    Removed the forEach methods from the Vertex class.

--- es6/graph/vertex.js
+++ es6/graph/vertex.js
@@ -40,14 +40,6 @@ class Vertex {
     this.incomingEdges.splice(index, 1);
   }
 
-  forEachIncomingEdge(callback) {
-    this.incomingEdges.forEach(callback);
-  }
-
-  forEachOutgoingEdge(callback) {
-    this.outgoingEdges.forEach(callback);
-  }
-
   static fromVertexName(vertexName) {
     const name = vertexName,  ///
           incomingEdges = [],
